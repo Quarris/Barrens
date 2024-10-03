5bb1e47da9a4ab1a04c7b820c53c2dc5c2dfb29a
Quarris <tomasz.winnicki@live.co.uk>
2024-10-03 04:51:39 +0100

    Initial commit

---
+++ src/main/kotlin/dev/quarris/wastelands/ModRef.kt
@@ -0,0 +1,14 @@
+package dev.quarris.wastelands
+
+import net.minecraft.resources.ResourceLocation
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+object ModRef {
+    const val ID: String = "wastelands"
+    val LOGGER: Logger = LogManager.getLogger(ID)
+
+    fun res(name: String): ResourceLocation {
+        return ResourceLocation.fromNamespaceAndPath(ID, name)
+    }
+}
---
+++ src/main/kotlin/dev/quarris/wastelands/Wastelands.kt
@@ -0,0 +1,8 @@
+package dev.quarris.wastelands
+
+import net.neoforged.fml.common.Mod
+
+@Mod(ModRef.ID)
+object Wastelands {
+
+}
